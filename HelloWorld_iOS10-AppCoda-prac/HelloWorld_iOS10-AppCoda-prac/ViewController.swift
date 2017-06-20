a1f16b5d69867d4b239e02eb0f1434d10d657a7a
陳維漢 <wikeyno@gmail.com>
2017-06-20 11:33:48 +0800

    first commit - hello world

---
+++ HelloWorld_iOS10-AppCoda-prac/HelloWorld_iOS10-AppCoda-prac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld_iOS10-AppCoda-prac
+//
+//  Created by 陳維漢 on 2017/6/20.
+//  Copyright © 2017年 陳維漢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
