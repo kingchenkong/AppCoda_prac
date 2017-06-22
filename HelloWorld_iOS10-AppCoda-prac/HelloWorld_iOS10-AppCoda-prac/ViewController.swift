482f4260e37b4a133e85756b16f948d0444582ba
陳維漢 <wikeyno@gmail.com>
2017-06-22 10:17:10 +0800

    Hello world-app is done. now Let’s go on chapter.6

--- HelloWorld_iOS10-AppCoda-prac/HelloWorld_iOS10-AppCoda-prac/ViewController.swift
+++ HelloWorld_iOS10-AppCoda-prac/HelloWorld_iOS10-AppCoda-prac/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK:    -IBAction
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+    }
 }
-
